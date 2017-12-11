1941dd80bfd1c2484ade3f80517a54d678ff779c
d-king27 <kingd8883@gmail.com>
2017-12-11 13:10:13 +0000

    altered App.js to be functional

--- src/componants/App.js
+++ src/componants/App.js
@@ -10,24 +10,22 @@ import Article from './Article'
 import UserPage from './UserPage'
 
 
+const App = ()=>{
+  return (
+    <Router>
+    <section className='card-panel  grey lighten-4'>
+      <div id='App' className='card-panel'>
+        <NavBar/>  
+        <Route exact path="/" component={MostPop} />
+    <Route path="/articles/:id" component={Article} />
+    <Route path="/topics/:id" component={TopicPage} />
+    <Route path="/user/:username" component={UserPage} />
+          
+      </div>
+    </section>
+    </Router> 
 
-class App extends React.Component {
-  render () {
-    return (
-      <Router>
-      <section className='card-panel  grey lighten-4'>
-        <div id='App' className='card-panel'>
-          <NavBar/>  
-          <Route exact path="/" component={MostPop} />
-      <Route path="/articles/:id" component={Article} />
-      <Route path="/topics/:id" component={TopicPage} />
-      <Route path="/user/:username" component={UserPage} />
-            
-        </div>
-      </section>
-      </Router> 
-    );
-  }
-}
+  )}
+
 
 export default App;
